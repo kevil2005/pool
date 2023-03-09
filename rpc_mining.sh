70df49e9e4d0c843f41d6a76431fa6069cc94338
kevil2005 <127235581+kevil2005@users.noreply.github.com>
2023-03-09 20:53:07 +0700

    Delete rpc_mining.sh

--- rpc_mining.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
-while :; do
-    ./astrominer -w dero1qygaqcw8dzfmckjy2wz0r4gyyca0ma3vguwlpr0kqayqq3u0fdfqkqgr2yu89 -r community-pools.mysrv.cloud:10300 -r1 dero.friendspool.club:10300 -r2 nodent2.cpumining.cloud:10100 -p rpc;
-    sleep 5;
-done
